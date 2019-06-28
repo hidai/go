008c3afbb3c7e08d8b3abb8ed489bbd5f4d3c8c0
Kenichi Hidai <hidai@nmn.jp>
2019-06-28 23:28:44 +0900

    Switch from go-html-transform/h5 to net/html

--- dom/dom.go
+++ dom/dom.go
@@ -1,40 +1,40 @@
 package dom
 
 import (
-	"code.google.com/p/go-html-transform/h5"
+	"golang.org/x/net/html"
 )
 
-func _getElementsByTagNameAndClass(e *h5.Node, tag_name string, class string, result *[]*h5.Node) {
-	if tag_name == "" || e.Data() == tag_name {
+func _getElementsByTagNameAndClass(e *html.Node, tag_name string, class string, result *[]*html.Node) {
+	if tag_name == "" || e.Data == tag_name {
 		if class == "" {
 			*result = append(*result, e)
 		} else {
 			for _, attr := range e.Attr {
-				if attr.Name == "class" && attr.Value == class {
+				if attr.Key == "class" && attr.Val == class {
 					*result = append(*result, e)
 				}
 			}
 		}
 	}
-	for _, child := range e.Children {
+	for child := e.FirstChild; child != nil; child = child.NextSibling {
 		_getElementsByTagNameAndClass(child, tag_name, class, result)
 	}
 }
 
-func GetElementsByTagNameAndClass(e *h5.Node, tag_name string, class string) []*h5.Node {
-	result := []*h5.Node{}
+func GetElementsByTagNameAndClass(e *html.Node, tag_name string, class string) []*html.Node {
+	result := []*html.Node{}
 	_getElementsByTagNameAndClass(e, tag_name, class, &result)
 	return result
 }
 
-func GetElementsByTagName(e *h5.Node, tag_name string) []*h5.Node {
-	result := []*h5.Node{}
+func GetElementsByTagName(e *html.Node, tag_name string) []*html.Node {
+	result := []*html.Node{}
 	_getElementsByTagNameAndClass(e, tag_name, "", &result)
 	return result
 }
 
-func GetElementsByClass(e *h5.Node, class string) []*h5.Node {
-	result := []*h5.Node{}
+func GetElementsByClass(e *html.Node, class string) []*html.Node {
+	result := []*html.Node{}
 	_getElementsByTagNameAndClass(e, "", class, &result)
 	return result
 }
